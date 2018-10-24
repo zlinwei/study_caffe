4d2ce6cf7ed4731ceef6cb44478dc4e833c3f743
linwei <zlinwei@zlinwei.com>
2018-10-24 08:01:27 +0800

    add

--- cnn/DeepNet.cpp
+++ cnn/DeepNet.cpp
@@ -6,9 +6,13 @@
 
 template<typename Dtype>
 void DeepNet<Dtype>::Init() {
-//    ::caffe::SolverParameter solver_param;
-//    ::caffe::ReadProtoFromTextFileOrDie(_solver_prototxt, &solver_param);
-//    _solver.reset(::caffe::SolverRegistry<Dtype>::CreateSolver(solver_param));
-//    assert(_solver);
+    ::caffe::SolverParameter solver_param;
+    ::caffe::ReadProtoFromTextFileOrDie(_solver_prototxt, &solver_param);
+    _solver.reset(::caffe::SolverRegistry<Dtype>::CreateSolver(solver_param));
+    assert(_solver);
 
 }
+
+
+
+INSTANTIATE_CLASS(DeepNet);
--- cnn/DeepNet.hpp
+++ cnn/DeepNet.hpp
@@ -13,26 +13,28 @@
 #include <caffe/caffe.hpp>
 #include <caffe/layers/memory_data_layer.hpp>
 
+
 template<typename Dtype>
 class DeepNet {
 public:
-//    using SolverSptr = boost::shared_ptr<::caffe::Solver<T>>;
+    using SolverSptr = boost::shared_ptr<::caffe::Solver<Dtype>>;
 
-//    using MemoryDataLayerSptr = boost::shared_ptr<::caffe::MemoryDataLayer<T>>;
+    using MemoryDataLayerSptr = boost::shared_ptr<::caffe::MemoryDataLayer<Dtype>>;
 
-    void Init();
+    virtual void Init();
 
 private:
-//    std::string _solver_prototxt = "solver.prototxt";
-//
-//    SolverSptr _solver;
-//
-//    MemoryDataLayerSptr _input_data_layer;
-//
-//    MemoryDataLayerSptr _input_label_layer;
+    std::string _solver_prototxt = "solver.prototxt";
+
+    SolverSptr _solver;
+
+    MemoryDataLayerSptr _input_data_layer;
+
+    MemoryDataLayerSptr _input_label_layer;
 
 
 };
+
 
 
 #endif //DEEPNET_HPP
